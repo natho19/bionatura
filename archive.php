e1bada627c0380a0d92d7f24818cfc675ec081f9
Nathan Akpawu <nakpawu@gmail.com>
2024-01-24 11:32:19 +0000

    archive projects and single project

---
+++ archive.php
@@ -0,0 +1,104 @@
+<?php get_header(); ?>
+
+<!----page header----->
+<div class="page_header_default style_one ">
+    <div class="parallax_cover">
+        <img src="<?= BIONATURA_IMG_URL . 'page-header-default.jpg'; ?>"  alt="bg_image" class="cover-parallax">
+    </div>
+    <div class="page_header_content">
+        <div class="auto-container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="banner_title_inner">
+                        <div class="title_page">Projets sociaux</div>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="breadcrumbs creote">
+                        <ul class="breadcrumb m-auto">
+                            <li><a href="http://localhost/bionatura/">Accueil</a></li>
+                            <li class="active">Projets sociaux</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!----page header----->
+
+<section class="project_all filt_style_four  filter_enabled">
+    <div class="container">
+        <!--===============spacing==============-->
+        <div class="pd_top_80"></div>
+        <!--===============spacing==============-->
+        <div class="project_all filt_style_three filter_enabled">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="fliter_group" style="text-align:center!important;">
+                    <ul class="project_filter dark clearfix">
+                        <li data-filter="*" class="current">Tout Voir</li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="project_container clearfix">
+                <div class="row clearfix">
+                    <div class="project-wrapper grid-item col-xl-4 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                        <div class="project_post style_one">
+                            <div class="image">
+                                <img width="1920" height="1052" src="<?= BIONATURA_IMG_URL . 'expertise-bg-1.jpg'; ?>" class="img-fluid" alt="img">
+                            </div>
+                            <div class="project_caro_content">
+                                <div class="left_side">
+                                    <h2 class="title_pro"><a href="http://localhost/bionatura/projets/projet-1-ipsum-dolor-sit-amet/" rel="bookmark">Complex Dismissal for a Small Company</a></h2>
+                                </div>
+                                <div class="right_side">
+                                    <a href="http://localhost/bionatura/projets/projet-1-ipsum-dolor-sit-amet/"><i class="icon-right-arrow"></i></a>
+                                    <a href="http://localhost/bionatura/projets/projet-1-ipsum-dolor-sit-amet/" class="two"><i class="icon-right-arrow"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="project-wrapper grid-item col-xl-4 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                        <div class="project_post style_one">
+                            <div class="image">
+                                <img width="1980" height="1320" src="<?= BIONATURA_IMG_URL . 'cal-to-action-2.jpg'; ?>" class="img-fluid" alt="img">
+                            </div>
+                            <div class="project_caro_content">
+                                <div class="left_side">
+                                    <h2 class="title_pro"><a href="http://localhost/bionatura/projets/projet-1-ipsum-dolor-sit-amet/" rel="bookmark">Essential Steps to Writing Job Description</a></h2>
+                                </div>
+                                <div class="right_side">
+                                    <a href="http://localhost/bionatura/projets/projet-1-ipsum-dolor-sit-amet/"><i class="icon-right-arrow"></i></a>
+                                    <a href="http://localhost/bionatura/projets/projet-1-ipsum-dolor-sit-amet/" class="two"><i class="icon-right-arrow"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="project-wrapper grid-item col-xl-4 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                        <div class="project_post style_one">
+                            <div class="image">
+                                <img width="1980" height="1320" src="<?= BIONATURA_IMG_URL . 'cal-to-action.jpg'; ?>" class="img-fluid" alt="img">
+                            </div>
+                            <div class="project_caro_content">
+                                <div class="left_side">
+                                    <h2 class="title_pro"><a href="http://localhost/bionatura/projets/projet-1-ipsum-dolor-sit-amet/" rel="bookmark">Company Values &amp; The Relationship</a></h2>
+                                </div>
+                                <div class="right_side">
+                                    <a href="http://localhost/bionatura/projets/projet-1-ipsum-dolor-sit-amet/"><i class="icon-right-arrow"></i></a>
+                                    <a href="http://localhost/bionatura/projets/projet-1-ipsum-dolor-sit-amet/" class="two"><i class="icon-right-arrow"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--===============spacing==============-->
+        <div class="pd_bottom_80"></div>
+        <!--===============spacing==============-->
+    </div>
+</section>
+
+<?php get_footer(); ?>
---
+++ single.php
@@ -0,0 +1,76 @@
+<?php get_header(); ?>
+
+<!----page header----->
+<div class="page_header_default style_one ">
+    <div class="parallax_cover">
+        <img src="<?= BIONATURA_IMG_URL . 'page-header-default.jpg'; ?>"  alt="bg_image" class="cover-parallax">
+    </div>
+    <div class="page_header_content">
+        <div class="auto-container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="banner_title_inner">
+                        <div class="title_page">Détails du projet</div>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="breadcrumbs creote">
+                        <ul class="breadcrumb m-auto">
+                            <li><a href="http://localhost/bionatura/">Accueil</a></li>
+                            <li class="active">Détails du projet</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!----page header----->
+
+<section class="project-detail-section">
+    <!--===============spacing==============-->
+    <div class="pd_top_80"></div>
+    <!--===============spacing==============-->
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-8 col-lg-9 col-md-12 mb-5 mb-md-5 mb-xl-0">
+                <div class="title_all_box style_one dark_color">
+                    <div class="title_sections left">
+                        <h2>Company Values & The Relationship</h2>
+                    </div>
+                </div>
+                <!--===============spacing==============-->
+                <div class="pd_bottom_10"></div>
+                <!--===============spacing==============-->
+                <div class="description_box">
+                    <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue equal blame belongs.</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi unde odio sint minus vitae sunt doloribus magnam facilis aperiam voluptates!</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus sit error eum hic. Id nulla velit veritatis, vero quisquam dignissimos eos amet, laborum reiciendis incidunt sit hic fugiat architecto iure?</p>
+                </div>
+                <div class="mr_top_40">
+                    <img class="img-fluid" src="<?= BIONATURA_IMG_URL . 'projects/project-1-img.jpg'; ?>" alt="Project Image">
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-3 col-md-12">
+                <div class="project_information">
+                    <h2>Information</h2>
+                    <div class="project_information_bo">
+                        <div class="repeat_informtion">
+                            <h6>Client: </h6>
+                            <p>The Sixmothers Group</p>
+                        </div>
+                        <div class="repeat_informtion">
+                            <h6>Location: </h6>
+                            <p>Philadelphia, United States</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+    <!--===============spacing==============-->
+    <div class="pd_bottom_80"></div>
+    <!--===============spacing==============-->                          
+</section>
+
+<?php get_footer(); ?>
