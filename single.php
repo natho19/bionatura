1bce6eb37b8051c0a1d3367b9e301ee44bf4a8c9
Nathan Akpawu <nakpawu@gmail.com>
2024-01-25 21:00:52 +0000

    dynamic single project

--- single.php
+++ single.php
@@ -7,38 +7,44 @@
     <div class="pd_top_80"></div>
     <!--===============spacing==============-->
     <div class="container">
-        <div class="row">
+        <div class="row justify-content-center">
             <div class="col-xl-8 col-lg-9 col-md-12 mb-5 mb-md-5 mb-xl-0">
                 <div class="title_all_box style_one dark_color">
                     <div class="title_sections left">
-                        <h2>Company Values & The Relationship</h2>
+                        <h2><?php the_title(); ?></h2>
                     </div>
                 </div>
                 <!--===============spacing==============-->
                 <div class="pd_bottom_10"></div>
                 <!--===============spacing==============-->
-                <div class="description_box">
-                    <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue equal blame belongs.</p>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi unde odio sint minus vitae sunt doloribus magnam facilis aperiam voluptates!</p>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus sit error eum hic. Id nulla velit veritatis, vero quisquam dignissimos eos amet, laborum reiciendis incidunt sit hic fugiat architecto iure?</p>
-                </div>
-                <img class="img-fluid single_project_image" src="<?= BIONATURA_IMG_URL . '900x600.png'; ?>" alt="Project Image">
+                <?php if (!empty(get_the_content())) : ?>
+                    <div class="description_box">
+                        <?php the_content(); ?>
+                    </div>
+                <?php endif; ?>
+                <img src="<?= has_post_thumbnail() ? get_the_post_thumbnail_url(get_the_ID(), 'bionatura900x600') : BIONATURA_IMG_URL . '900x600.png'; ?>" class="img-fluid single_project_image" alt="<?= esc_attr(get_the_title()); ?>">
             </div>
-            <div class="col-xl-4 col-lg-3 col-md-12">
-                <div class="project_information">
-                    <h2>Information</h2>
-                    <div class="project_information_bo">
-                        <div class="repeat_informtion">
-                            <h6>Client: </h6>
-                            <p>The Sixmothers Group</p>
-                        </div>
-                        <div class="repeat_informtion">
-                            <h6>Location: </h6>
-                            <p>Philadelphia, United States</p>
+            <?php if (get_field('project_client') || get_field('project_location')) : ?>
+                <div class="col-xl-4 col-lg-3 col-md-12">
+                    <div class="project_information">
+                        <h2>Information</h2>
+                        <div class="project_information_bo">
+                            <?php if (get_field('project_client')) : ?>
+                                <div class="repeat_informtion">
+                                    <h6>Client :</h6>
+                                    <p><?php the_field('project_client'); ?></p>
+                                </div>
+                            <?php endif; ?>
+                            <?php if (get_field('project_location')) : ?>
+                                <div class="repeat_informtion">
+                                    <h6>Localité :</h6>
+                                    <p><?php the_field('project_location'); ?></p>
+                                </div>
+                            <?php endif; ?>
                         </div>
                     </div>
                 </div>
-            </div>
+            <?php endif; ?>
         </div>
     </div>    
     <!--===============spacing==============-->
